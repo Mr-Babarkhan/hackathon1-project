fb97fb1ac5535b9cd84ad4454bec840cbe98d25d
Mr-Babarkhan <muhammadbabarkhan813@gmail.com>
2023-08-16 22:36:36 -0700

    update slider

--- next.config.js
+++ next.config.js
@@ -1,35 +1,9 @@
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "bk-ecommerce.vercel.app",
-                port: "",
-                pathname: "/**",
-            },
-            {
-                protocol: "https",
-                hostname: "cdn.sanity.io",
-                port: "",
-                pathname: "/**",
-            },
-            // {
-            //     protocol: "https",
-            //     hostname: "todo-application-ten.vercel.app",
-            //     port: "",
-            //     pathname: "/**",
-            // },
-            // {
-            //     protocol: "https",
-            //     hostname: "lh3.googleusercontent.com",
-            //     port: "",
-            //     pathname: "/**",
-            // },
-        ],
+    images:{
+        domains:['cdn.sanity.io']
     }
 }
 
 module.exports = nextConfig
+
